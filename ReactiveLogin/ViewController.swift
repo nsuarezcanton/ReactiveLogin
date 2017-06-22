a1aded6fd1323bac318e1ebdf9b2238160906b1f
Nico Suarez-Canton Trueba <nsuarez.canton@gmail.com>
2017-06-22 13:20:35 +0200

    Initial Commit

---
+++ ReactiveLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReactiveLogin
+//
+//  Created by Nicolas Suarez-Canton Trueba on 6/22/17.
+//  Copyright © 2017 Nicolas Suarez-Canton Trueba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
